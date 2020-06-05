b515e7b6756daac60052419bedf313a5fb7998ac
yash-flutter <yash-flutter@users.noreply.github.com>
2020-06-05 15:04:11 +0530

    http

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
-
-import 'package:firstapp/change_name_card.dart';
+import 'package:http/http.dart' as http;
 import 'package:firstapp/drawer.dart';
-
+import 'dart:convert';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _name = TextEditingController(); 
+  
+  TextEditingController _namecontroller = TextEditingController();
+  
   var mytext="Change Me";
- 
+  var data;
+  var url="https://jsonplaceholder.typicode.com/photos";
+
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async{
+    var res= await http.get(url);
+    data=jsonDecode(res.body);
   }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar:AppBar(title: Text("Awesome App"),),
-      
+      appBar: AppBar(title: Text("Awesome App"),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          mytext=_namecontroller.text;
+          setState(() {
+          });
+        },
+        child: Icon(Icons.edit),
+        ),
+      drawer: MyDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-                  child: Card(
-                    color: Colors.cyanAccent,
-                    elevation: 20,
-            child:ChangeNameCard(mytext: mytext, name: _name) ,),
-        ),
-      ),
-      
-      
-      floatingActionButton: FloatingActionButton(
-        
-        onPressed: (){
-          mytext=_name.text;
-          setState(() {});
-        },
-        child: Icon(Icons.get_app),
-        
-      ),
-      
-      drawer: MyDrawer()
-      
+        child: data!=null?Card(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+            return ListTile(title: Text(data[index]["title"],),
+                            leading: Image.network(data[index]["url"]),
+                            
+            );
+           },
+          ),
+        ):Center(child: CircularProgressIndicator()),
+      ),    
     );
   }
 }
